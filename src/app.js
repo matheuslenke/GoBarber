5d2020efc4e2522341df5a0b009e8b900cc76124
Matheus Lenke <matheuslenke@MacBook-Air-de-Matheus.local>
2019-11-03 16:45:06 -0300

    Installing eslint

--- src/app.js
+++ src/app.js
@@ -2,18 +2,20 @@ import express from 'express';
 import routes from './routes';
 
 class App {
-    constructor() {
-        this.server = express();
-        this.middlewares();
-        this.routes();
-    }
-    middlewares() {
-        this.server.use(express.json());
-    }
+  constructor() {
+    this.server = express();
+    this.middlewares();
+    this.routes();
+  }
 
-    routes() {
-        this.server.use(routes);
-    }
+  middlewares() {
+    this.server.use(express.json());
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
 }
 
-export default new App().server;
+export default new App().server
+;
