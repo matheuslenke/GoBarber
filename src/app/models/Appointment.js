5b846310c922c992a387f5721da3d9ac468e581f
matheuslenke <matheus.l.coutinho@aluno.ufes.br>
2019-11-18 16:07:03 -0300

    appointments working

---
+++ src/app/models/Appointment.js
@@ -0,0 +1,26 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Appointment extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        date: Sequelize.DATE,
+        canceled_at: Sequelize.DATE,
+      },
+      {
+        sequelize,
+      }
+    );
+
+    return this;
+  }
+
+  static associate(models) {
+    // Quando se faz mais de um relacionamento para a mesma tabela, é obrigatório definir
+    // Um apelido para cada foreignKey
+    this.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' });
+    this.belongsTo(models.User, { foreignKey: 'provider_id', as: 'provider' });
+  }
+}
+
+export default Appointment;
--- src/database/index.js
+++ src/database/index.js
@@ -2,10 +2,11 @@ import Sequelize from 'sequelize';
 
 import User from '../app/models/User';
 import File from '../app/models/File';
+import Appointment from '../app/models/Appointment';
 
 import databaseConfig from '../config/database';
 
-const models = [File, User];
+const models = [File, User, Appointment];
 
 class Database {
   constructor() {
