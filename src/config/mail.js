14267d6e37d2166a1938ac48fa5b0c91d5411db9
matheuslenke <matheus.l.coutinho@aluno.ufes.br>
2019-11-22 10:34:15 -0300

    Email config

---
+++ src/config/mail.js
@@ -0,0 +1,23 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'cda173e88ed2fd',
+    pass: '465317c719a18f',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
+
+/**
+ * Serviços de email:
+ * Amazon SES
+ * Mailgun
+ * Sparkpost
+ * Mandril(Mailchimp)
+ * GMAIL
+ *
+ * Mailtrap (Apenas desenvolvimento)
+ */
