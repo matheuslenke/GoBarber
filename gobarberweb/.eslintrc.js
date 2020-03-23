c8d9851ae7e47fc80fe893ffbca9b6dda6817380
matheuslenke <matheus.l.coutinho@aluno.ufes.br>
2020-03-23 17:54:57 -0300

    Starting app

--- gobarberweb/.eslintrc.js
+++ gobarberweb/.eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
     SharedArrayBuffer: "readonly",
     __DEV__: true
   },
+  parser: 'babel-eslint',
   parserOptions: {
     ecmaFeatures: {
       jsx: true
@@ -36,7 +37,8 @@ module.exports = {
   settings: {
     "import/resolver": {
       "babel-plugin-root-import": {
-        rootPathSuffix: "src"
+        "rootPathPrefix": "@",
+        "rootPathSuffix": "src"
       },
     },
   },
