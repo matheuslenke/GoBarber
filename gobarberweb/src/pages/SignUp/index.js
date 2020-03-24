44ddd10fede285235aff15a5fe6d3e9fb48eb205
matheuslenke <matheus.l.coutinho@aluno.ufes.br>
2020-03-23 21:30:22 -0300

    feat: signUp page

--- gobarberweb/src/pages/SignUp/index.js
+++ gobarberweb/src/pages/SignUp/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '~/assets/logo.svg';
 
 // import { Container } from './styles';
 
-export default function SignUp() {
-  return <div />;
+export default function SignIn() {
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <form>
+        <input placeholder="Nome completo" />
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Sua senha" />
+
+        <button type="submit">Acessar</button>
+        <Link to="/">Já tenho um login</Link>
+      </form>
+    </>
+  );
 }
