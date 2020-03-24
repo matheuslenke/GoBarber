82a24de49bc0e1d90b53cc245cabe9da7c050587
matheuslenke <matheus.l.coutinho@aluno.ufes.br>
2020-03-23 21:51:15 -0300

    feat: signin and signup validation

--- gobarberweb/src/pages/SignUp/index.js
+++ gobarberweb/src/pages/SignUp/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
 import logo from '~/assets/logo.svg';
 
-// import { Container } from './styles';
+const schema = Yup.object().shape({
+  name: Yup.string().required('O nome é obrigatório'),
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('O email é obrigatório'),
+  password: Yup.string()
+    .required('A senha é obrigatória')
+    .min(6, 'No mínimo 6 caracteres'),
+});
 
 export default function SignIn() {
   function handleSubmit(data) {
@@ -15,7 +24,7 @@ export default function SignIn() {
     <>
       <img src={logo} alt="GoBarber" />
 
-      <Form onSubmit={handleSubmit}>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="name" placeholder="Nome completo" />
         <Input name="email" type="email" placeholder="Seu e-mail" />
         <Input name="password" type="password" placeholder="Sua senha" />
